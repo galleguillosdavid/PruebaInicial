0d716e8d84f37c4e30decbea35e069e4e10436b7
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-10-05 16:35:53 -0300

    Por segmentar dibujo 2 fragmento

--- app/src/main/java/com/example/retrofit/UI/General/FragmentZero.kt
+++ app/src/main/java/com/example/retrofit/UI/General/FragmentZero.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.retrofit.R
+import kotlinx.android.synthetic.main.fragment_zero.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,5 +58,12 @@ class FragmentZero : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Personajes.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentZero_to_FirstFragment)
+        }
     }
 }
